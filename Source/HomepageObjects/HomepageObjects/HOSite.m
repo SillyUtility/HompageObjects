8468cda0952cbb5c46c6af5b5e2972c219f543e4
Eddie Hillenbrand <eddie@graphdyne.com>
2021-03-05 20:42:02 -0800

    Initialize new site from template

--- Source/HomepageObjects/HomepageObjects/HOSite.m
+++ Source/HomepageObjects/HomepageObjects/HOSite.m
@@ -43,9 +43,80 @@
         }
     }
 
-    // TODO: populate directory
+    [self initializeSiteDirectory:dir withTemplate:@"DefaultSite"];
 
     return nil;
+}
+
++ (void)initializeSiteDirectory:(NSString *)siteDirectory
+    withTemplate:(NSString *)template
+{
+    NSFileManager *fm;
+    NSBundle *bundle;
+    NSString *templatePath;
+
+    fprintf(stderr, "Initializing %s\n", siteDirectory.UTF8String);
+
+    fm = NSFileManager.defaultManager;
+    bundle = [NSBundle bundleForClass:self];
+
+    templatePath = [bundle
+        pathForResource:[NSString stringWithFormat:@"Templates/%@", template]
+        ofType:@""
+    ];
+
+    [self copyConentsAtPath:templatePath toPath:siteDirectory];
+}
+
+static void log_indent(NSUInteger level, const char *fmt, const char *arg) {
+    while (level--)
+        fprintf(stderr, "  ");
+    fprintf(stderr, fmt, arg);
+}
+
++ (void)copyConentsAtPath:(NSString *)srcPath toPath:(NSString *)destPath
+{
+    NSFileManager *fm;
+    NSDirectoryEnumerator *enumerator;
+    NSString *path, *newDir,
+        *fullSrcPath, *fullDestPath;
+    BOOL isDir;
+    NSError *err;
+
+    fm = NSFileManager.defaultManager;
+    enumerator = [fm enumeratorAtPath:srcPath];
+
+    while ((path = enumerator.nextObject)) {
+        fullSrcPath = [srcPath stringByAppendingPathComponent:path];
+
+        if (![fm fileExistsAtPath:fullSrcPath isDirectory:&isDir])
+            continue;
+
+        if (isDir) {
+            log_indent(enumerator.level, "mkdir %s\n", path.UTF8String);
+            newDir = [destPath stringByAppendingPathComponent:path];
+            // TODO: allow directory renames
+            [fm createDirectoryAtPath:newDir
+                withIntermediateDirectories:YES
+                attributes:nil
+                error:&err
+            ];
+            if (err) {
+                fprintf(stderr, "failed to create %s\n", newDir.UTF8String);
+                return;
+            }
+            continue;
+        }
+
+        log_indent(enumerator.level, "cp %s\n", path.UTF8String);
+        fullDestPath = [destPath stringByAppendingPathComponent:path];
+        // TODO: process files (something like replace $$VAR_NAME$$)
+        [fm copyItemAtPath:fullSrcPath toPath:fullDestPath error:&err];
+        if (err) {
+            fprintf(stderr, "failed to copy %s\n", path.UTF8String);
+            return;
+        }
+    }
 }
 
 @end
