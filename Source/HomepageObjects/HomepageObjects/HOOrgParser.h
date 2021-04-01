d578466d8e5ac3ada2679b2e4570e5c195b26b1b
Eddie Hillenbrand <eddie@graphdyne.com>
2021-03-31 21:35:00 -0700

    Org-mode parser stubs

---
+++ Source/HomepageObjects/HomepageObjects/HOOrgParser.h
@@ -0,0 +1,51 @@
+//
+//  HOOrgParser.h
+//  HomepageObjects
+//
+//  Created by Eddie Hillenbrand on 3/31/21.
+//  Copyright © 2021 Silly Utility LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol HOOrgParserDelegate;
+
+/*
+ * The Emacs Org-mode parser.
+ */
+@interface HOOrgParser : NSObject
+
+- initWithJSONAtPath:(NSString *)path;
+
+@property (nullable, assign) id <HOOrgParserDelegate> delegate;
+
+- (BOOL)parse;
+- (void)abortParsing;
+
+@property (nullable, readonly, copy) NSError *parserError;
+
+@end
+
+
+/*
+ * The methods a delegate may implement to respond to Org-mode parse
+ * events.
+ */
+@protocol HOOrgParserDelegate <NSObject>
+@optional
+
+- (void)parserDidStartDocument:(HOOrgParser *)parser;
+- (void)parserDidEndDocument:(HOOrgParser *)parser;
+
+- (void)parser:(HOOrgParser *)parser didStartNode:(NSString *)nodeType
+    reference:(NSString *)ref
+    properties:(NSDictionary<NSString *, id> *)properties;
+- (void)parser:(HOOrgParser *)parser didEndNode:(NSString *)nodeType;
+
+- (void)parser:(HOOrgParser *)parser parseError:(NSString *)message;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Source/HomepageObjects/HomepageObjects/HOOrgParser.m
@@ -0,0 +1,33 @@
+//
+//  HOOrgParser.m
+//  HomepageObjects
+//
+//  Created by Eddie Hillenbrand on 3/31/21.
+//  Copyright © 2021 Silly Utility LLC. All rights reserved.
+//
+
+#import "HOOrgParser.h"
+
+@implementation HOOrgParser
+
+- initWithJSONAtPath:(NSString *)path
+{
+    if (!(self = [super init]))
+        return self;
+
+    // ...
+
+    return self;
+}
+
+- (BOOL)parse
+{
+    return NO;
+}
+
+- (void)abortParsing
+{
+
+}
+
+@end
